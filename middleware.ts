558d4925399a9a74340440545988e1d160e69e94
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2024-11-03 17:37:28 +0530

    udpate on Fixed bug

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,18 @@ import { getToken } from 'next-auth/jwt';
 
 export async function middleware(request: NextRequest) {
   // Get the session using the request object
-  //const session = await getServerSession(authOptions);
+
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+ const session = await getServerSession(authOptions);
   // Redirect to login if no valid session is found
-  if (!token) {
+ 
+  if(!token || !session) {
     return NextResponse.redirect(new URL('/auth/login', request.url));
   }
 
   // Log the session for debugging
-  console.log('Session:', token);
+  console.log('token:', token);
+  console.log('Session:', session);
 
   return NextResponse.next();
 }
